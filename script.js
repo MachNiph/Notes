19d7e50d5875681d78c487a3de14d753a6ea513b
redshirtfull456 <nishanphuyal111@gmail.com>
2023-07-05 12:17:16 +0545

    changes

--- script.js
+++ script.js
@@ -1,31 +1,55 @@
 const notesContainer = document.querySelector(".container");
-const createBtn = document.querySelector(".create-notes");
+let notes = document.querySelectorAll(".input-text");
 
-createBtn.addEventListener("click", () => {
-  const inputField = document.createElement("div");
-  inputField.classList.add("input-field");
+function showNotes() {
+  notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes();
 
-  const inputBox = document.createElement("p");
-  inputBox.classList.add("input-text");
-  inputBox.setAttribute("contenteditable", "true");
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.classList.add("delete-btn");
-
-  const deleteImg = document.createElement("img");
-  deleteImg.src = "Images/delete.png";
-
-  deleteBtn.appendChild(deleteImg);
-  inputField.appendChild(inputBox);
-  inputField.appendChild(deleteBtn);
-  notesContainer.appendChild(inputField);
-});
+function updateStorage() {
+  localStorage.setItem("notes", notesContainer.innerHTML);
+}
 
 notesContainer.addEventListener("click", function (e) {
   if (e.target.tagName === "IMG") {
     var grandparentElement = e.target.parentElement.parentElement;
     if (grandparentElement.classList.contains("input-field")) {
       grandparentElement.remove();
-    }
+      updateStorage();
+    } else return;
+  } else if (e.target.classList.contains("input-text")) {
+    notes = document.querySelectorAll(".input-text");
+    notes.forEach((item) => {
+      item.addEventListener("keyup", function () {
+        updateStorage();
+      });
+    });
+  }
+
+  if (e.target.classList.contains("create-notes")) {
+    const inputField = document.createElement("div");
+    inputField.classList.add("input-field");
+
+    const inputBox = document.createElement("p");
+    inputBox.classList.add("input-text");
+    inputBox.setAttribute("contenteditable", "true");
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("delete-btn");
+
+    const deleteImg = document.createElement("img");
+    deleteImg.src = "Images/delete.png";
+
+    deleteBtn.appendChild(deleteImg);
+    inputField.appendChild(inputBox);
+    inputField.appendChild(deleteBtn);
+    notesContainer.appendChild(inputField);
+  }
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    document.execCommand("insertLineBreak");
+    event.preventDefault();
   }
 });
