9679d461dc323012e580750477b2727c56d32a71
redshirtfull456 <nishanphuyal111@gmail.com>
2023-07-06 07:42:14 +0545

    change

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function showNotes() {
   if (storedNotes) {
     notesContainer.innerHTML = storedNotes;
   } else {
-    notesContainer.innerHTML = "No notes found";
+    notesContainer.innerHTML = notesContainer;
   }
 }
 
